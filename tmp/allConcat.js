d3c453751afebac259d46cf8a30d4a5323f453c3
Evan <stewarea@gmail.com>
2016-10-14 09:20:33 -0700

    add some variables from the get

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -7,10 +7,16 @@ $(document).ready(function() {
     var user = $('#user').val();
     $('#user').val("");
     $.get('https://api.github.com/users/'+ user +'?access_token=' + apiKey).then(function(response) {
-      console.log(response);
+      var userid = response.login;
+      var name = response.name;
+      var photo = response.avatar_url;
+      var repositorties = response.public_repos
+      $('.userid').text("User ID: " + user);
+      $('.name').text(" Name: " + name);
+      $('.photo').text("Photo: " + "<img src=" + photo + ">");
+      $('.repos').text("Public Repos: " + repositories);
      }).fail(function(error){
        console.log(error.responseJSON.message);
      });
-     console.log(apiKey);
   });
 });
