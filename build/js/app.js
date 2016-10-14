56c66de7a2f814849b31172c561c3462dc7b3374
Evan <stewarea@gmail.com>
2016-10-14 10:02:09 -0700

    add photo and repo properties and some styling to display

--- build/js/app.js
+++ build/js/app.js
@@ -11,14 +11,18 @@ $(document).ready(function() {
     var user = $('#user').val();
     $('#user').val("");
     $.get('https://api.github.com/users/'+ user +'?access_token=' + apiKey).then(function(response) {
+      $(".result").show();
       var userid = response.login;
       var name = response.name;
       var photo = response.avatar_url;
-      var repositorties = response.public_repos
+      var repositories = response.public_repos;
+      var bio = response.bio;
       $('.userid').text("User ID: " + user);
       $('.name').text(" Name: " + name);
-      $('.photo').text("Photo: " + "<img src=" + photo + ">");
+      $('.photo').html("<img src="+ photo +">");
+      $('.bio').text(bio);
       $('.repos').text("Public Repos: " + repositories);
+      console.log(repositories);
      }).fail(function(error){
        console.log(error.responseJSON.message);
      });
